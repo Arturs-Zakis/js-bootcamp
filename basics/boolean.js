e19118340712778d720e25b7d5ecf0968f93fc31
Artūrs <arturs11228@gmail.com>
2020-01-11 20:24:15 +0200

    lesson 14

---
+++ basics/boolean.js
@@ -0,0 +1,13 @@
+// let temp = 31;
+// let isFreezing = temp < 32 ;
+
+
+
+// console.log(isFreezing);
+
+let age = 70;
+let isChild = age <= 7;
+let isSenior = age >= 65;
+
+console.log('is child: ' + isChild);
+console.log('is senior: ' + isSenior);
